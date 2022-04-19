75915d3383a432525adca6557d67fc8d9ddd8bd7
Sanjit01 <sanjitthakuri6750@gmail.com>
2022-04-19 07:59:46 +0545

    Reducers added in user and cart context

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,4 +1,6 @@
-import React, { useState, createContext, useEffect } from "react";
+import React, { createContext, useReducer } from "react";
+
+import { CreateAction } from "../utils/reducer/reducer.utils";
 
 const addCartItem = (cartItems = [], productToAdd) => {
   // if car items contain product to add
@@ -76,37 +78,82 @@ export const CartContext = createContext({
   cartTotalPrice: 0,
 });
 
-export const CartProvider = ({ children }) => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
-  const [cartCount, setCartCount] = useState(0);
-  const [cartTotalPrice, setCartTotalPrice] = useState(0);
+export const CART_ACTION_TYPES = {
+  SET_SHOW_CART: "SET_SHOW_CART",
+  SET_CART_ITEMS: "SET_CART_ITEMS",
+};
 
-  useEffect(() => {
-    const newCartCount = cartItems.reduce((total, cartItem) => {
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  cartTotalPrice: 0,
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case CART_ACTION_TYPES.SET_SHOW_CART:
+      return {
+        ...state,
+        isCartOpen: payload,
+      };
+    case CART_ACTION_TYPES.SET_CART_ITEMS:
+      return {
+        ...state,
+        ...payload,
+      };
+
+    default:
+      throw new Error(`Unhandled type ${type} in user reducer`);
+  }
+};
+
+export const CartProvider = ({ children }) => {
+  // const [isCartOpen, setIsCartOpen] = useState(false);
+  const [{ isCartOpen, cartCount, cartItems, cartTotalPrice }, dispatch] =
+    useReducer(cartReducer, INITIAL_STATE);
+
+  // const [cartItems, setCartItems] = useState([]);
+  // const [cartCount, setCartCount] = useState(0);
+  // const [cartTotalPrice, setCartTotalPrice] = useState(0);
+
+  const updateCartReducer = (newCartItems) => {
+    const newCartCount = newCartItems.reduce((total, cartItem) => {
       return total + cartItem.quantity;
     }, 0);
-    setCartCount(newCartCount);
-  }, [cartItems]);
 
-  //total price
-  useEffect(() => {
-    const newTotalPrice = cartItems.reduce((total, cartItem) => {
+    const newTotalPrice = newCartItems.reduce((total, cartItem) => {
       return total + cartItem.quantity * cartItem.price;
     }, 0);
-    setCartTotalPrice(newTotalPrice);
-  }, [cartItems]);
+
+    dispatch(
+      CreateAction(CART_ACTION_TYPES.SET_CART_ITEMS, {
+        cartItems: newCartItems,
+        cartTotalPrice: newTotalPrice,
+        cartCount: newCartCount,
+      })
+    );
+  };
+
+  // show cart dropdown
+  const setIsCartOpen = (bool) => {
+    dispatch(CreateAction(CART_ACTION_TYPES.SET_SHOW_CART, bool));
+  };
 
   const addItemToCart = (productToAdd) => {
-    setCartItems(addCartItem(cartItems, productToAdd));
+    const newCartItems = addCartItem(cartItems, productToAdd);
+    updateCartReducer(newCartItems);
   };
   // remove item
   const removeItemFromCart = (productToRemove) => {
-    setCartItems(removeCartItem(cartItems, productToRemove));
+    const newCartItems = removeCartItem(cartItems, productToRemove);
+    updateCartReducer(newCartItems);
   };
 
   const decreaseItemCountFromCart = (productToRemove) => {
-    setCartItems(decreaseCartItemCount(cartItems, productToRemove));
+    const newCartItems = decreaseCartItemCount(cartItems, productToRemove);
+    updateCartReducer(newCartItems);
   };
 
   const value = {
--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,6 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
+
+import { CreateAction } from "../utils/reducer/reducer.utils";
 
 import {
   onAuthStateChangedListener,
@@ -13,13 +15,35 @@ export const UserContext = createContext({
   setCurrentUser: () => null,
 });
 
-/**
- * The UserProvider function is a React component that returns a UserContext.Provider component with
- * the children prop set to the children prop that was passed into the UserProvider function.
- * @returns The UserProvider component is being returned.
- */
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in user reducer`);
+  }
+};
+
+const INITIAL_STATE = { currentUser: null };
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  // const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+
+  console.log(currentUser);
+
+  const setCurrentUser = (user) => {
+    dispatch(CreateAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
